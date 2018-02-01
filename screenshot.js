e88f0e2bd0842380f420e69167f941d75233f776
Steve Baker <ste.baker.92@gmail.com>
2018-02-01 13:59:58 +0000

    Added a mock form for submitting

--- screenshot.js
+++ screenshot.js
@@ -5,3 +5,29 @@
 function setScreenshotUrl(url) {
   document.getElementById('target').src = url;
 }
+
+document.getElementById("btnSubmit").addEventListener("click", submit);
+
+document.getElementById("selCategory").onchange = function (event) {
+  document.getElementById("selSubCategory").style.display = event.target.selectedIndex === 2 ? "inline-block" : "none";
+}
+
+function submit() {
+  console.log("submitting ... ");
+
+  if (document.getElementById('target').src.includes("white.png")) {
+    // user has refreshed
+    console.warn("screenshot missing");
+    return;
+  }
+
+  var button = document.getElementById("btnSubmit");
+  var text = document.getElementById("txtReason").value;
+
+  console.log("reason: " + text);
+
+  button.disabled = true;
+
+  button.innerText = "Submitted!";
+
+}
